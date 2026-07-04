f7b6b78cfa383bba7574f3ec72c63bb760603655
SIMMY-KUMARI12 <simmykumari825@gmail.com>
2026-07-04 12:21:58 +0530

    EntityFrameWorkCore done

--- EntityFrameWorkCore/RetailInventory/Program.cs
+++ EntityFrameWorkCore/RetailInventory/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+
+using var context = new AppDbContext();
+
+// Retrieveing all products
+var products = await context.Products.ToListAsync();
+
+Console.WriteLine("All Products:");
+
+foreach (var p in products)
+{
+    Console.WriteLine($"{p.Name} - ₹{p.Price}");
+}
+
+Console.WriteLine();
+
+// Finding product by ID
+var product = await context.Products.FindAsync(1);
+
+Console.WriteLine($"Found: {product?.Name}");
+
+Console.WriteLine();
+
+// first expensive product
+var expensive = await context.Products.FirstOrDefaultAsync(p => p.Price > 50000);
+
+Console.WriteLine($"Expensive: {expensive?.Name}");
