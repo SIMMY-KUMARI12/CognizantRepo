24daf05de56e7c7da1039a0ea3e35236d6edc16e
SIMMY-KUMARI12 <simmykumari825@gmail.com>
2026-07-26 23:16:20 +0530

    Handon 4 task1 done

--- Angular/student-course-portal/src/app/app.routes.ts
+++ Angular/student-course-portal/src/app/app.routes.ts
@@ -1,19 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
-import { CoursesComponent } from './pages/courses/courses';
-import { ProfileComponent } from './pages/profile/profile';
-
+import { EnrollmentFormComponent } from './pages/enrollment-form/enrollment-form';
 export const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'courses',
-    component: CoursesComponent
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent
-  }
+
+    path: 'enroll',
+  component: EnrollmentFormComponent  
+
+}
 ];
